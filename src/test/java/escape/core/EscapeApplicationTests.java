9ab730ff4732196de88422afad18345be0066990
Yeriimii <seojs0511@naver.com>
2024-09-13 20:40:50 +0900

    Initial commit

---
+++ src/test/java/escape/core/EscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package escape.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
