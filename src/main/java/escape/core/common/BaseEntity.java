fd10a84acfefef8769a618e4034c06d329dade6d
Yeriimii <seojs0511@naver.com>
2024-09-16 21:48:10 +0900

    feat: BaseEntity 작성
    - 생성 시간, 수정 시간 뿐만 아니라 생성자, 수정자를 추적할 수 있음

---
+++ src/main/java/escape/core/common/BaseEntity.java
@@ -0,0 +1,23 @@
+package escape.core.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity extends BaseTimeEntity {
+
+    @CreatedBy
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private String updatedBy;
+}
